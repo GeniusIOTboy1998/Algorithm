76486ea20b117aa1857224dc4c354071e5ef3022
叶荣荣 <43310819+GeniusIOTboy1998@users.noreply.github.com>
2019-03-21 15:48:44 +0800

    001.cpp

---
+++ string/001.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+#include<iostream>
+
+using namespace std;
+
+char s[] = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+
+int main() {
+	int i, c;
+	while ((c = getchar()) != EOF) {   
+		for (i = 1; s[i] && s[i] != c; i++);
+		if (s[i]) putchar(s[i - 1]);
+		else putchar(c);
+	}
+	system("pause");
+	return 0;
+}
